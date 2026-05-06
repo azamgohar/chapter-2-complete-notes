97ac530ffc7f92c4d9f35f1d9d4cd9bd385ade06
Azam Gohar <144214140+azamgohar@users.noreply.github.com>
2026-05-06 23:43:17 +0500

    Add files via upload

---
+++ primitive and reference types/typeof Quirks/script.js
@@ -0,0 +1,7 @@
+// typeof quirks(e.g.,typeof null = 'object')
+
+// quirk is basically some wierd things in JS like the data type  null's type, in actual  is object. but it's still primitive so it's confusing and wierd but for now we just have to remember these kinds of few quirks. and some of them are here: 
+
+// 1 + "1" = 11; 
+// typeof NaN is Number  // NaN clearly means that Not a Number but it's type is still Number!
+// and so much more... so I have to search them and remember them
