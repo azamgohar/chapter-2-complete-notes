94092354939bb70035d92f6d1889a7f7160f4911
Azam Gohar <144214140+azamgohar@users.noreply.github.com>
2026-05-06 23:33:59 +0500

    Add files via upload

---
+++ primitive and reference types/primitive  types/string/script.js
@@ -0,0 +1,12 @@
+// Strings ✅
+// '' single quotes
+// "" double quotes
+// `` backticks
+
+// all these three signs helps creating a String, If anything is wrapped by anyone of them then it is a string (group of alphabetic characters.) 
+
+
+let myFirstName = "'A'zam";
+let myFullName = `${myFirstName} Gohar`;
+console.log(myFirstName);
+console.log(myFullName);
