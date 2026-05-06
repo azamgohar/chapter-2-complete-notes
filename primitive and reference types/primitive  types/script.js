daf80dd83574db25fbd9f9f5e53e1b6836ab856c
Azam Gohar <144214140+azamgohar@users.noreply.github.com>
2026-05-06 23:11:00 +0500

    Add files via upload

---
+++ primitive and reference types/primitive  types/script.js
@@ -0,0 +1,14 @@
+
+
+// Primitives ✅
+// values which does not have any kind of brackets like strings or numbers or booleans and there are so much more... and primitives can be copied directly.
+
+// forexample:
+
+// let a = 12;
+// let  b = a;
+
+// a = a + 2;
+
+// yahan per a ki value 12 hai aur b ki value mein a ki value copied hai aur aage chal ke maine apne a ki value change ki hai to b ki value per koi asar nahi hoga kyunki b mein  already a ki pehli value  copy ho ke aa chuki hai phir baad mein a ki value change hone se b per koi fark nahi padega. so it is primitive type data.
+
