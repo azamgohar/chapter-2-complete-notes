a5773d78dac2c7666ecb80a42f1f831cd9cb6d95
Azam Gohar <144214140+azamgohar@users.noreply.github.com>
2026-05-06 23:38:40 +0500

    Add files via upload

---
+++ primitive and reference types/primitive  types/undefined/script.js
@@ -0,0 +1,4 @@
+
+
+// Undefined ✅
+// If we have created a variable without initializing it's value so the variable exists but because we didn't gave any value to that, JS declare  it's value as undefined. Js itself gives it a value undefined because JS don't let any variable stay without a value so it gaves every non-initialized variable a value undefined.
