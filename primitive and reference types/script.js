adce9a3d7e0de0a924b13a4fb0b69828ebba22e0
Azam Gohar <144214140+azamgohar@users.noreply.github.com>
2026-05-06 23:05:56 +0500

    Add files via upload

---
+++ primitive and reference types/script.js
@@ -0,0 +1,12 @@
+// Data types = Data's types
+
+// Data could be: 
+// string number boolean null undefined Symbol NaN BigInt
+// azam  12 true null undefined Symbol [] {} () NaN BigInt and so on...
+
+// Data have so many types but there are mainly two types:
+// 1 Primitives
+
+// 2 References
+
+
