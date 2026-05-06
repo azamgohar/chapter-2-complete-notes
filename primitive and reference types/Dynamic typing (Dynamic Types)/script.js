d6580fd9f7eb504b6ec97b137da8cccdb3afd12b
Azam Gohar <144214140+azamgohar@users.noreply.github.com>
2026-05-06 23:09:20 +0500

    Add files via upload

---
+++ primitive and reference types/Dynamic typing (Dynamic Types)/script.js
@@ -0,0 +1,17 @@
+// Dynamic Typing (Dynamic Types)
+
+// in dynamic typing, the word typing refers to types not words typing.
+
+// in almost all of other languages we have to mention the variable's value  type with declaration and we can't change that type! Example: 
+// int a = 12; // ✅
+// a = true; // ❌
+// we can't do this in almost  all other languages because they  don't allow to  change  their  variable's data types after declaration. and that is  Static Typing. But in Js we  don't need  to mention the  variable's data type and we can change it whenever and however we want! Example:
+
+// let a = 12; // ✅
+// a =  true; // ✅
+// a = "Azam"; // ✅
+// a = {}; // ✅
+// a = null; // ✅
+// a = undefined;  // ✅
+
+// any of them will not create  any problem so it means we can  change the variable's data type after declaration. and this is what Dynamic Typing is.
