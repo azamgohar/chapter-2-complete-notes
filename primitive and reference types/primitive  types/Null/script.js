22ce3f6ac4a4ed5b7107182d4aa21349d3ade376
Azam Gohar <144214140+azamgohar@users.noreply.github.com>
2026-05-06 23:24:14 +0500

    Add files via upload

---
+++ primitive and reference types/primitive  types/Null/script.js
@@ -0,0 +1,14 @@
+
+
+// Null ✅
+// Null basically  use to declare a variable empty, example:
+// let selectedCars  = null;
+// I have 5cars and I said I will choose one of themfor tonight's drive, so here I didn't had selected one but I know that I will select one later. so the selectedCar variable is empty for now and when I'll decide orselect one of my cars it's name will goes in it!
+// selectedCars  = "Hybrid";
+
+
+
+let jar = null;
+
+jar = "Tea Leaves"
+console.log(jar);
