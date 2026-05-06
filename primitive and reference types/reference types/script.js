e72172e66d320181171d39a06a23bc56a9ebf368
Azam Gohar <144214140+azamgohar@users.noreply.github.com>
2026-05-06 23:39:48 +0500

    Add files via upload

---
+++ primitive and reference types/reference types/script.js
@@ -0,0 +1,16 @@
+
+
+// References✅
+// values which have brackets like arrays, objects and functions and references can't be copied directly. but we'll get the reference from it's parent(the variable)!
+
+// let a = [1, 2, 3];
+// let b = a;
+// b.pop();
+
+
+// pop basically removes one  last valuefrom an object so here I have popped the last value of b variable which looks like having the copied  values of a but it's not like that when we'll check b's values now it will be 1,  2 and that's what we did but If we'll check a's value it  is also 1, 2 because b is  using a's reference which means Ifanything will happens to a or b any one of them  itwill  also affect on the other one.
+
+// for example a ki  apni gaadi hai aur b ne a kicopy karke apni ek gaadi karli to wo primitive hai aur agar a ne b ko keh diya ke aaj se meri gaadi teri bhi hai aur a ne gaadithodi matlab b ki bhi thuki aur b  ne thoki to a ki bhi thuki, Gaadi.
+
+// so every bracket value is reference value 💥
+
