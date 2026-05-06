45258e69b956386891febcdf978a9e0fe924466a
Azam Gohar <144214140+azamgohar@users.noreply.github.com>
2026-05-06 23:42:22 +0500

    Add files via upload

---
+++ primitive and reference types/type  coercion/script.js
@@ -0,0 +1,10 @@
+// Type Coercion ✅
+
+// for example we're adding "5" into 1:
+"5" + 1; 
+// the answer will be "51" because in maths + works to add but in programming + do 2 things, addition and concatenation(concate) which means joinning not adding.
+// Js reads the code and when it see that with any single one of the operands nearby our operator (operator means + - * / and others and operands are  values  with  it) is a string then it converts every operand into string and concatenate all of them.
+
+"5" - 1;
+
+// here  it goes change because only + do 2 works not - minus simply just do subtraction so the answer will  be 4.
