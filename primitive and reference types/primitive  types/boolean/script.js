e8c753e15f765625b7513afde840f12c822517ab
Azam Gohar <144214140+azamgohar@users.noreply.github.com>
2026-05-06 23:16:19 +0500

    Add files via upload

---
+++ primitive and reference types/primitive  types/boolean/script.js
@@ -0,0 +1,13 @@
+
+// Booleans ✅
+// basically true and false are booleans! for example:
+// let a = true;
+// let b = false;
+// here, the values of a and b both are boolean type data!
+
+if (true) {
+    console.log("Heeeyyyy!");
+}
+else {
+    console.log("Heeellllooooo!");
+}
