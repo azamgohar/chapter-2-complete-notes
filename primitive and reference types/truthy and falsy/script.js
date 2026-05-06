20c532200584c81e5e6ee9bd1d5a06137677b313
Azam Gohar <144214140+azamgohar@users.noreply.github.com>
2026-05-06 23:40:42 +0500

    Add files via upload

---
+++ primitive and reference types/truthy and falsy/script.js
@@ -0,0 +1,27 @@
+// Truthy and Falsy ✅
+
+// in conditionals we create conditions which that If this will happen do that and If that will happen then do this, so these are conditions and when we use them we have to create conditions which will converts into true or falsy, which  means they have only two behaviors truthy and falsy.  for example we have an If conditional statement
+
+if(false) {
+
+}
+
+// if statement - conditional (condition)
+let a = 15;
+if(a>20) {
+    console.log("Tum jeetey!");
+}
+else if(a<20) {
+    console.log("Tie ho gaya!");
+}
+else  {
+    console.log("Main jeeta!");
+}
+
+// inside the simple brackets of the conditional statement we set our condition,  and If our condition is one of these:
+
+// 0 false "" null undefined NaN document.all       ---> false
+
+// then the behavior of our condition will be falsy. And every other thing is there even integers in - values that is still true because that is not from the list up there, only thesedefined few are falsy  behaviors conditions. and all others are truthy!
+
+// rest all ---> truthy
