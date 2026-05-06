7f3d9ff641d75e33b7cf7441a0ea778b4eee1bd4
Azam Gohar <144214140+azamgohar@users.noreply.github.com>
2026-05-06 23:19:45 +0500

    Add files via upload

---
+++ primitive and reference types/primitive  types/Difference  Between Null and Undefined/script.js
@@ -0,0 +1,3 @@
+
+// Difference Bettwen Null and Undefined ✅
+// in Null we set our variable's value as empty (null) but in undefined's case If we have not given a value to some variable JS automatically  gives it a value undefined by  it's own self!
